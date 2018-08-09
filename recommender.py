3d765a0a5cdc8c01e1bb7e5f819f9be98c7ceaef
david <davidpierrealves21@gmail.com>
2018-08-09 01:10:33 -0300

    Added some other distances to the algorithm

--- recommender.py
+++ recommender.py
@@ -11,10 +11,9 @@ theuser = 6041
 def media(numbers, n = 2):
     return (sum(numbers) + (3.0 * n)) / float(len(numbers) + n)
 
-def euclidiana(rating1, rating2, r = 2):
+def minkowski(rating1, rating2, r = 2):  #Minkowski / Manhattan / Euclidiana ---- Basta modificar o valor de r 
     """
     Verifica a distancia das avaliacoes dos filmes
-    utilizando a distancia euclidiana(Ou minkowski com raiz quadrada)
     """
     
     distance = 0
@@ -28,7 +27,57 @@ def euclidiana(rating1, rating2, r = 2):
     else:
         return 1000000 #Quando o usuario nao tem avaliacoes em comum
 
-distance = euclidiana
+
+
+def pearson(rating1, rating2): # Coeficiente de correlacao de Pearson
+    soma_xy = 0
+    soma_x = 0
+    soma_y = 0
+    soma_x2 = 0
+    soma_y2 = 0
+    n = 0
+    for key in rating1:
+        if key in rating2:
+            n += 1
+            x = rating1[key]
+            y = rating2[key]
+            soma_xy += x * y
+            soma_x += x
+            soma_y += y
+            soma_x2 += x ** 2
+            soma_y2 += y ** 2
+            
+    # para calcular o denominador
+    if n == 0:
+        return 1000000
+    denominador = (sqrt(soma_x2 - (soma_x**2) / n) *
+                  sqrt(soma_y2 -(soma_y**2) / n))
+    if denominador == 0:
+        return 100000000
+    else:
+        sim = (soma_xy - (soma_x * soma_y) / n) / denominador
+        return 1.0 - sim
+
+
+def lmg_rmse(rating1, rating2): #erro quadratico medio (root mean square error)
+    max_rating = 5.0
+    sum = 0
+    count = 0
+    for (key, rating) in rating1.items():
+        if key in rating2:
+            sum += (rating2[key] - rating) ** 2
+            count += 1
+
+    if not count:
+        return 1000000 
+
+    return sqrt(sum / float(count)) + (max_rating / count)
+
+
+
+distance = minkowski # escolho qual tipo de distancia usar.
+
+
     
 # --- Carrega as avaliacoes dos usuarios
 users = {} # userid -> {movie : rating, movie : rating, ...}
